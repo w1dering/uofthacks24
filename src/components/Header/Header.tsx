0d3d3621b02695559f58b46264fd2fcfbac1cc6a
KenGao 0216 <ken.gao0216@gmail.com>
2025-01-17 23:10:31 -0500

    Header formatting

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,19 @@
 import "./Header.css";
+import logo from "../../assets/LogoTemp.png"
 
 const Header = () => {
-    return <div id="header">WOOOOOOOOO</div>
+    return (
+    <div id="header">
+        
+    <img src ={logo} alt="Logo"></img>
+    <div id = "AI-description">
+        <h3>AI Name+Description</h3> 
+    </div>
+        <div id = "project-name">
+            <h2>Project Name</h2>
+        </div>
+    </div>
+    )
 }
 
 export default Header;
